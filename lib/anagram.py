1bfa1acd08d9212a36bb5636ad8b13e0d17aa50f
Wambui <nderituwambui93@gmail.com>
2024-06-18 00:39:36 +0300

    updating labs

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
-# your code goes here!
+class Anagram:
+    '''Class Anagram in anagram.py'''
+
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, candidates):
+        return [word
+                for word in candidates
+                if self.word.lower() != word.lower()
+                and sorted(self.word.lower()) == sorted(word.lower())]
